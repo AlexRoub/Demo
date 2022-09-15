b054db2f689142f56c20b801fdf01970e39102d1
aroubeidis <aroubeidis@gmail.com>
2022-09-15 12:14:59 +0300

    feature first commit

--- src/main/java/org/roumpeal/Main.java
+++ src/main/java/org/roumpeal/Main.java
@@ -3,5 +3,6 @@ package org.roumpeal;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!! This is Alex Roub");
+        System.out.println("I want to show you a new feature!");
     }
 }
