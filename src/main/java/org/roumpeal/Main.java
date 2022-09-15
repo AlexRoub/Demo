59da60a408514cd0ee8e00188e672a57bea485d6
aroubeidis <aroubeidis@gmail.com>
2022-09-15 12:11:30 +0300

    second commit

--- src/main/java/org/roumpeal/Main.java
+++ src/main/java/org/roumpeal/Main.java
@@ -2,6 +2,6 @@ package org.roumpeal;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        System.out.println("Hello world!! This is Alex Roub");
     }
 }
