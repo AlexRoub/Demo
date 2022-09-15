1a76bdfaa151746bd56d5b4db8da2bdac30126fd
aroubeidis <aroubeidis@gmail.com>
2022-09-15 12:16:59 +0300

    feature second commit

--- src/main/java/org/roumpeal/Main.java
+++ src/main/java/org/roumpeal/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!! This is Alex Roub");
         System.out.println("I want to show you a new feature!");
+        System.out.println("This is the new feature!");
     }
 }
