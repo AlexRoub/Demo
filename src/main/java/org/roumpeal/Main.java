26210756ce8b8d942f9dbf215993417df9d937ef
aroubeidis <aroubeidis@gmail.com>
2022-09-15 13:30:44 +0300

    main class updates

--- src/main/java/org/roumpeal/Main.java
+++ src/main/java/org/roumpeal/Main.java
@@ -2,15 +2,10 @@ package org.roumpeal;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!! This is Alex Roub");
-        System.out.println("I want to show you a new feature!");
-        System.out.println("This is the new feature!");
-        System.out.println("This is the updated feature!");
-        System.out.println("This is the addition!");
-        System.out.println("This is the addition 1!");
-        System.out.println("This is the addition 2!");
+        System.out.println("\nHello world!! This is Alex Roub");
+        System.out.println("I want to show you a new car!");
 
-        Car car = new Car("Tesla X Y", 5);
-        System.out.println("Model is" + car.getModel() + " and it has " + car.getDoors() + " doors.");
+        Car car = new Car("Tesla X", 5);
+        System.out.println("Model is " + car.getModel() + " and it has " + car.getDoors() + " doors!");
     }
 }
