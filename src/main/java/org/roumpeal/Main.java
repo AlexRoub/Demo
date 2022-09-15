e2f833042b7f3118f7f09b6549a6befffb0324e2
aroubeidis <43012205+AlexRoub@users.noreply.github.com>
2022-09-15 12:56:09 +0300

    Update Main.java

--- src/main/java/org/roumpeal/Main.java
+++ src/main/java/org/roumpeal/Main.java
@@ -7,6 +7,5 @@ public class Main {
         System.out.println("This is the new feature!");
         System.out.println("This is the updated feature!");
         System.out.println("This is the addition!");
-        System.out.println("This is the addition 2!");
     }
 }
