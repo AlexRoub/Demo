f7f746f3eaf1df740af83f3cc8d010d7d4416391
aroubeidis <aroubeidis@gmail.com>
2022-09-15 12:28:14 +0300

    added updates on feature

--- src/main/java/org/roumpeal/Main.java
+++ src/main/java/org/roumpeal/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!! This is Alex Roub");
         System.out.println("I want to show you a new feature!");
         System.out.println("This is the new feature!");
+        System.out.println("This is the updated feature!");
     }
 }
